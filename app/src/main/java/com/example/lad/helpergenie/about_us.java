715a8e6cee5fa85e92f9787e4cc5de5f8323cae2
Pranav Vyas <pranavvyas4399@gmail.com>
2018-01-19 23:28:49 +0530

    Created Navigation Fragment

---
+++ app/src/main/java/com/example/lad/helpergenie/about_us.java
@@ -0,0 +1,24 @@
+package com.example.lad.helpergenie;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Pro on 1/19/2018.
+ */
+
+public class about_us extends Fragment {
+
+    View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.about_us_layout,container,false);
+        return mView;
+    }
+}
