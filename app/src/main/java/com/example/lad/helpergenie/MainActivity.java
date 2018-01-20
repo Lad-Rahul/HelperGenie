6e13e89c3053635fe4e7a3ce646b4a373cf380e3
Lad-Rahul <rahullad945@gmail.com>
2018-01-20 11:46:29 +0530

    Add android google services

--- app/src/main/java/com/example/lad/helpergenie/MainActivity.java
+++ app/src/main/java/com/example/lad/helpergenie/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.lad.helpergenie;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        fragmentManager =  getFragmentManager();
+        fragmentManager =  getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.alternatingLayout,new homeActivity()).commit();
     }
 
@@ -74,7 +75,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
             return true;
         }
 
@@ -86,7 +87,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        fragmentManager =  getFragmentManager();
+        fragmentManager =  getSupportFragmentManager();
 
         if (id == R.id.nav_home) {
             fragmentManager.beginTransaction().replace(R.id.alternatingLayout,new homeActivity()).commit();
