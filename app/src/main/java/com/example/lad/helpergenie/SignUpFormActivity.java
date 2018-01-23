bd87fdf4cf2816c2022a642b18eb524b5dd40981
Lad-Rahul <rahullad945@gmail.com>
2018-01-23 23:11:34 +0530

    Form Validation and Get & Add Pincode to Spinner

--- app/src/main/java/com/example/lad/helpergenie/SignUpFormActivity.java
+++ app/src/main/java/com/example/lad/helpergenie/SignUpFormActivity.java
@@ -4,7 +4,9 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +23,7 @@ public class SignUpFormActivity extends AppCompatActivity {
     private TextView FnameText;
     private FirebaseDatabase mData;
     private DatabaseReference mRef;
+    private Button btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +50,42 @@ public class SignUpFormActivity extends AppCompatActivity {
         Fadd1 = (EditText)findViewById(R.id.fadd1);
         Fadd2 = (EditText)findViewById(R.id.fadd2);
         Fpin = (EditText)findViewById(R.id.fpin);
-
+        btnReg = (Button)findViewById(R.id.btn_fregister);
 
         FnameText.setText("Current User : "+MainActivity.CurrUser);
+
+
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmobile = Fmobile.getText().toString();
+                String tadd1 = Fadd1.getText().toString();
+                String tadd2 = Fadd2.getText().toString();
+                String tpin = Fpin.getText().toString();
+                if( tmobile.matches("")){
+                    Toast.makeText(SignUpFormActivity.this,"Write Mobile No",Toast.LENGTH_SHORT).show();
+                }
+                else if(tadd1.matches("")){
+                    Toast.makeText(SignUpFormActivity.this,"Write Address line 1",Toast.LENGTH_SHORT).show();
+                }
+                else if(tadd2.matches("")){
+                    Toast.makeText(SignUpFormActivity.this,"Write Address line 2",Toast.LENGTH_SHORT).show();
+                }
+                else if(tpin.matches("")){
+                    Toast.makeText(SignUpFormActivity.this,"Write Pincode",Toast.LENGTH_SHORT).show();
+                }
+                else if(tpin.length() != 6){
+                    Toast.makeText(SignUpFormActivity.this,"Enter Proper Pincode",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    sendData();
+                }
+            }
+        });
     }
 
-    public void sendData(View view) {
+    public void sendData() {
         String userEmail = MainActivity.MainCurrUserEmail;
         String userKey = userEmail.replace(".","");
         String userName = MainActivity.CurrUser;
--- app/src/main/java/com/example/lad/helpergenie/homeActivity.java
+++ app/src/main/java/com/example/lad/helpergenie/homeActivity.java
@@ -10,6 +10,15 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * Created by Pro on 1/19/2018.
  */
@@ -18,6 +27,9 @@ public class homeActivity extends Fragment {
 
     View mView;
     Spinner searchSP;
+    Spinner searchPin;
+    private FirebaseDatabase mData;
+    private DatabaseReference mRef;
 
     @Nullable
     @Override
@@ -30,6 +42,44 @@ public class homeActivity extends Fragment {
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         searchSP.setAdapter(arrayAdapter);
 
+        searchPin = (Spinner)mView.findViewById(R.id.search_PIN);
+
+        mData = FirebaseDatabase.getInstance();
+        mRef = mData.getReference().child("pincode");
+        //final ArrayList<String> pincodeList = new ArrayList<>();
+
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                    collectPincode((Map<String,Object>) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         return mView;
+    }
+
+    private void collectPincode(Map<String,Object> users){
+
+        ArrayList<Map> pinObjectList = new ArrayList<>();
+        ArrayList<String> pinList = new ArrayList<>();
+
+        for(Map.Entry<String,Object> entry : users.entrySet()){
+
+            String SinglePin = (String)entry.getKey();
+            Map SingleUser = (Map)entry.getValue();
+
+            pinList.add(SinglePin);
+            pinObjectList.add(SingleUser);
+        }
+
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(this.getActivity(),android.R.layout.simple_spinner_item,pinList);
+        arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        searchPin.setAdapter(arrayAdapter2);
+
     }
 }
