34e22fb7d6e86a4044b8872098dff1f9a07e0ac2
Lad-Rahul <rahullad945@gmail.com>
2018-01-29 21:40:44 +0530

    Get Service Provider Information

--- app/src/main/java/com/example/lad/helpergenie/get_sp.java
+++ app/src/main/java/com/example/lad/helpergenie/get_sp.java
@@ -3,6 +3,7 @@ package com.example.lad.helpergenie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,11 +20,15 @@ public class get_sp extends AppCompatActivity {
     private FirebaseDatabase mData;
     private DatabaseReference mRef;
     private String name,proffesion,email,mobile,SPpro;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_sp);
 
+        textView = (TextView)findViewById(R.id.textView2);
+
+        final ArrayList<FireSP> ListSP2 = new ArrayList<>();
         ArrayList<String> ListSP = new ArrayList<>();
         String newString;
         if (savedInstanceState == null) {
@@ -32,7 +37,7 @@ public class get_sp extends AppCompatActivity {
                 newString = null;
             } else {
                 ListSP = extras.getStringArrayList("Service Provider at this Location");
-                SPpro = extras.getString("Service Provider Proffesion");
+                SPpro = extras.getString("Service Provider Proffesion").toString();
                 Toast.makeText(get_sp.this,SPpro,Toast.LENGTH_SHORT).show();
             }
         }
@@ -40,6 +45,7 @@ public class get_sp extends AppCompatActivity {
         for(int i=0;i<ListSP.size();i++){
             Toast.makeText(get_sp.this,ListSP.get(i).toString(),Toast.LENGTH_SHORT).show();
         }
+
 
         mData = FirebaseDatabase.getInstance();
 
@@ -55,13 +61,16 @@ public class get_sp extends AppCompatActivity {
                     email = fireSP.getEmail();
                     mobile = fireSP.getMobile();
                     proffesion = fireSP.getProffesion();
-//
+
 //                    Log.d("hello1","name :" + name);
 //                    Log.d("hello1","email :" + email);
 //                    Log.d("hello1","mobile :" + mobile);
 //                    Log.d("hello1","proffesion :" + proffesion);
 
-                    FireSP i = new FireSP(email,mobile,name,proffesion);
+                    if(proffesion.equals(SPpro)){
+                        ListSP2.add(fireSP);
+                    }
+                    //FireSP i = new FireSP(email,mobile,name,proffesion);
                 }
 
                 @Override
@@ -71,5 +80,17 @@ public class get_sp extends AppCompatActivity {
             };
             mRef.addValueEventListener(valueEventListener);
         }
+        FireSP temp;
+        for(int i=0;i<ListSP2.size();i++){
+            temp = ListSP2.get(i);
+            name = temp.getName();
+            email = temp.getMobile();
+            mobile = temp.getMobile();
+            proffesion = temp.getProffesion();
+
+            textView.append(name + email + mobile + proffesion);
+            Log.d("hello1",name + email + mobile + proffesion);
+        }
+
     }
 }
