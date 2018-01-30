b9a843e44a56eeefd2f8eb8a09532fe5cfdfb2c0
Pranav Vyas <pranavvyas4399@gmail.com>
2018-01-30 23:00:26 +0530

    Created new Display Activity

---
+++ app/src/main/java/com/example/lad/helpergenie/displaySP.java
@@ -0,0 +1,13 @@
+package com.example.lad.helpergenie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displaySP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_sp);
+    }
+}
