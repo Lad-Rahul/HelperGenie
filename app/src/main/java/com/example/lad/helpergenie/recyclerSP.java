66e0587cbf7a62bed6be6f89ba75410165c7530f
Pranav Vyas <pranavvyas4399@gmail.com>
2018-02-01 21:57:02 +0530

    Added Call Functionality in Service Provider Page

--- app/src/main/java/com/example/lad/helpergenie/recyclerSP.java
+++ app/src/main/java/com/example/lad/helpergenie/recyclerSP.java
@@ -1,11 +1,16 @@
 package com.example.lad.helpergenie;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.net.URI;
 
 /**
  * Created by Pro on 1/31/2018.
@@ -36,10 +41,18 @@ public class recyclerSP extends RecyclerView.Adapter<recyclerSP.HolderClass> {
 
     @Override
     public void onBindViewHolder(HolderClass holder, int position) {
+        final HolderClass finalHolder = holder;
         holder.name.setText(name[position]);
         holder.emailID.setText(email[position]);
         holder.mobile.setText(mobile[position]);
         holder.ID.setText(id[position]);
+        holder.phoneCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + finalHolder.mobile.getText().toString()));
+                ct.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -50,6 +63,7 @@ public class recyclerSP extends RecyclerView.Adapter<recyclerSP.HolderClass> {
     public class HolderClass extends RecyclerView.ViewHolder {
 
         TextView name,emailID,mobile,ID;
+        ImageView phoneCall;
 
         public HolderClass(View itemView) {
             super(itemView);
@@ -57,6 +71,7 @@ public class recyclerSP extends RecyclerView.Adapter<recyclerSP.HolderClass> {
             emailID = (TextView) itemView.findViewById(R.id.spEmail);
             mobile = (TextView) itemView.findViewById(R.id.spMobile);
             ID = (TextView) itemView.findViewById(R.id.spID);
+            phoneCall = (ImageView) itemView.findViewById(R.id.phoneCall);
         }
     }
 
