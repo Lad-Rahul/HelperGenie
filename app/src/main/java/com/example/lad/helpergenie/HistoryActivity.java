e6e4ad39a0c8248563ecfb02c86a5b403e28760e
Pranav Vyas <pranavvyas4399@gmail.com>
2018-02-08 16:12:24 +0530

    -Fixing Bugs

--- app/src/main/java/com/example/lad/helpergenie/HistoryActivity.java
+++ app/src/main/java/com/example/lad/helpergenie/HistoryActivity.java
@@ -1,10 +1,25 @@
 package com.example.lad.helpergenie;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by LAD on 20-Jan-18.
  */
 
 public class HistoryActivity extends Fragment {
+    View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.activity_history,container,false);
+
+
+        return mView;
+    }
 }
